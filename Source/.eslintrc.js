48fbd4315be302b4cadc198e4f1ac35a355d6940
4nds <andrej.slapnicar@gmail.com>
2022-10-22 16:05:35 +0200

    Removed unnecessary and conflicting rules

--- Source/.eslintrc.js
+++ Source/.eslintrc.js
@@ -12,29 +12,19 @@ module.exports = {
   rules: {
     // These are organized by the order in which they appear in the ESLint docs.
 
+    //
     // Best Practices
 
-    // C++: Warn about empty return values.
+    // Warn about empty return values.
     "consistent-return": "warn",
-    // C++: Require type-safe comparisons.
+    // Require type-safe comparisons.
     eqeqeq: "error",
-    // C++: Warn about unused expressions.
+    // Warn about unused expressions.
     "no-unused-expressions": "warn",
-    // Style: Allow fallthrough in switches.
-    "no-fallthrough": "off",
 
-    // Variables
-
-    // C++: Warn for unused variables.
-    "no-unused-vars": "warn",
-
+    //
     // Stylistic Issues
 
-    // Style: Force array newlines IF there are any existing newlines (Everything being on one line,
-    // and it being a short line is alright.).
-    "array-bracket-newline": "warn",
-    // Style: Use Allman style braces, warn otherwise.
-    "brace-style": ["warn", "allman"],
     // Style: Use 2 levels of indentation, throw error otherwise..
     indent: ["error", 2],
     // Consistency: Use template literals, throw error otherwise.
@@ -43,9 +33,10 @@ module.exports = {
     "max-len": ["error", 100],
     // Style: Use spaces only.
     "no-tabs": "error",
-    // C++: Use semicolons.
+    // Use semicolons.
     semi: "error",
 
+    //
     // EMCAScript 6
 
     // Use const when possible.
